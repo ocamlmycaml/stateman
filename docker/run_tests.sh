9471a2e263885c1e0dd24862bb170a626380f2a3
Aditya Dhara <adhara@wayfair.com>
2018-07-30 19:46:07 -0400

    Added travis

--- docker/run_tests.sh
+++ docker/run_tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Running pytest..."
-pytest --cov-report html --cov=stateman ./tests
+pytest --cov-report html --cov-report term --cov=stateman ./tests
 
 echo "Running flake8..."
 flake8
